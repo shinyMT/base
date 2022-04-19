7900844971e1947d6d601c1625ee3b98fb903cc0
59865 <1664125539@qq.com>
2022-04-19 15:53:26 +0800

    删除不必要依赖和文件

---
+++ src/main/java/BaseItem.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+/**
+ * Author: thy
+ * Date: 2022/4/19 15:48
+ */
+public class BaseItem implements Serializable {
+    private static final long serialVersionUID = 2537508678090056505L;
+}
