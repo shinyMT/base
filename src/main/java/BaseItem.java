516b6a026104e0859a64d0eb637528fa0de2e1c9
59865 <1664125539@qq.com>
2022-04-19 16:09:10 +0800

    修改base

--- src/main/java/BaseItem.java
+++ src/main/java/BaseItem.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
  * Author: thy
  * Date: 2022/4/19 15:48
  */
-public class BaseItem implements Serializable {
+public abstract class BaseItem implements Serializable {
     private static final long serialVersionUID = 2537508678090056505L;
+    public BaseItem(){
+
+    }
 }
